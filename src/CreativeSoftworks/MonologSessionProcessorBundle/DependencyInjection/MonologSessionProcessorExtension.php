85c440892cffb90971d7f01a4dee97fb5e9b73d1
Ricardo Oliveira <ricardo.oliveira@creative-softworks.com>
2014-06-27 15:03:33 +0100

    corrected wrong namespace

--- src/CreativeSoftworks/MonologSessionProcessorBundle/DependencyInjection/MonologSessionProcessorExtension.php
+++ src/CreativeSoftworks/MonologSessionProcessorBundle/DependencyInjection/MonologSessionProcessorExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Vm\WebSafeActiveChoiceBundle\DependencyInjection;
+namespace CreativeSoftworks\MonologSessionProcessorBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
